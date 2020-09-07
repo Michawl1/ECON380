b6cca5ae051435477fb8433513f042be3cdc066e
Michael Thompson <mjt106@case.edu>
2020-09-06 20:25:09 -0400

    more homework 2 work

--- Project2/project2.py
+++ Project2/project2.py
@@ -16,22 +16,23 @@ with open('project2_data.json') as f:
 # Section 2 Extract information from the dataset, 
 # 2-1. create a dictionary 'guyprefer' contains mens' preferences
 guyprefers = json_data['men_preference']
-    
+
 # 2-2. create a dictionary 'galprefer' contains women's preferences
 galprefers = json_data['women_preference']
-    
+
 # 2-3. create a list contains guys who are currently not engaged, 
 # sort alphabetically
-free_guy = 
+free_guy = list(guyprefers)
+free_guy.sort()
 
 # 2-4. generate an empty dictionary 'engage_book' to store result
-engage_book = 
+engage_book = {}
 
 # 2-5. make copies of guyprefers and gal refers
-guypreference = .copy()
+guypreference = guyprefers.copy()
+galpreference = galprefers.copy()
 
-
-# Section 3. Impletement the Gale-Shapley algorithm 
+# Section 3. Impletement the Gale-Shapley algorithm
 # Follow the algorithm flowchart, it should be very helpful
 
 # pop the first guy in the free_guy list, let him take the move
@@ -39,15 +40,12 @@ a_brave_guy = free_guy.pop(0)
 # get his preference list
 mylist = guypreference[a_brave_guy].copy()
 # let this guy take the move
-while  mylist:
+while mylist:
     # Let's propose to my favorate lady!
     my_girl = mylist.pop(0)
     # YOU WILL NEED TO DO THE REST, GOOD LUCK AND HAVE FUN!.
 
-
-
 # Section 4 (optional). Stability check
 # define stability: there are no two people of opposite sex who would both
 # rather have each other than their current partners.
-"""
 
