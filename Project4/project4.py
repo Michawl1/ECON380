4feba487e711c71d0813dacaa51cc99211f41873
Michael Thompson <mjt106@case.edu>
2020-09-23 21:17:00 -0400

    Adding finished code

--- Project4/project4.py
+++ Project4/project4.py
@@ -23,19 +23,20 @@ n_j = np.array([18000, 22000, 50100, 21010, 25000])
 # 1.3 use the random integer generation function in numpy to create a 5 x 5
 # Origination-Destination flow matrix
 # Set the lower limit to 1000, upper limit to 3,000
-od_matrix = np.random.randint(1000, 3001, (5, 5))
-# 1.4 same modal share across all regions (1)
+od_matrix = np.random.randint(1000, 3000, size=(5, 5))
+
+# 1.4 same modal share across all regione (1)
 alpha_vec = np.full(len(n_j), 1)
 # 1.5 same transmission rate across all regions
 beta_vec = np.full(len(n_j), 0.8)
 # 1.6 same recover rate  across all regions
-gamma_vec = np.full(len(n_j), 0.05)
+gamma_vec = np.full(len(n_j), 0.03)
 # 1.7 normal o-d flow
-od_flow = 1
+od_flow = np.round(od_matrix) * 1
 # 1.8 Have 300 iterations
 days = 300
-
-fatality_vec = np.zeros(5)
+# 1.9 same fatality rate across all regions
+fatality_vec = np.full(len(n_j), 0.01)
 
 
 # =============================================================================
@@ -45,18 +46,19 @@ fatality_vec = np.zeros(5)
 # infected proportion in areas are:
 # areaA: 1%; areaB: 0.5%; areaC:0.1%; arerD:0%, areaE:0%
 
-def start_detect(n_j, infected, immune):
-    sir = np.zeros(shape=(3, 5))
-
-    for i in range(0, n_j.size):
-        sir[1][i] = infected[i] * n_j[i]
-        sir[0][i] = n_j[i] - sir[1][i]
-        sir[2][i] = immune[i]
-
+def start_detect(n_j, detect, immune):
+    sir = np.zeros(shape=(5, 4))
+    sir[:, 0] = n_j
+    init_infect = np.round(sir[:, 0] * detect)
+    init_immune = np.round(sir[:, 0] * immune)
+    sir[:, 0] = sir[:, 0] - init_infect - init_immune
+    sir[:, 1] = sir[:, 1] + init_infect
+    sir[:, 2] = sir[:, 2] + init_immune
     return sir
 
 
-sir = start_detect(n_j, [0.01, 0.005, 0.01, 0, 0], [0, 0, 0, 0, 0])
+sir = start_detect(n_j, np.array([0.01, 0.005, 0.001, 0, 0]), 0)
+
 
 # =============================================================================
 # Section 3. Define a function to simulate the epidemic in this big region
@@ -117,39 +119,64 @@ def epidemic_sim(n_j, initial_status, od_flow,
 
     # 3.3. use total_days as the iterator
     for j in range(0, days):
-        s = []
-        inf = []
-        r = []
-        d = []
+        ##3.3 figure out where those infected people go
 
-        for i in range(0, n_j.size):
-            new_infect = sir_sim[0, i] * beta_vec[i]
-            if new_infect > sir_sim[0, i]:
-                new_infect = sir_sim[0, i]
+        # normalize the sir table by calculating the percentage of each group
+        sir_percent = sir_sim / n_j[:, np.newaxis]
 
-            new_recovered = sir_sim[1, i] * gamma_vec[i]
-            sir_sim[0, i] -= new_infect
-            sir_sim[1, i] += new_infect
-            sir_sim[1, i] = sir_sim[0, 1] - new_recovered
+        # assuming the infected people travel to all ares with the same probability:
+        infected_mat = np.array([sir_percent[:, 1], ] * len(n_j)).transpose()
+        # od_infected gives the flow of infected people. i.e., where they go
+        od_infected = np.round(od_flow * infected_mat)
 
-            sir_sim[2, i] += new_recovered
-            if sir_sim[0, i] < 0:
-                sir_sim[0, i] = 0
-            if sir_sim[1, i] < 0:
-                sir_sim[1, i] = 0
-            if sir_sim[2, i] < 0:
-                sir_sim[2, i] = 0
+        # "inflow infected" are those who will spread the disease to susceptible
+        inflow_infected = od_infected.sum(axis=0)  # total infected inflow in each area
+        inflow_infected = np.round(inflow_infected * alpha_vec)
 
-            s.append(sir_sim[0, i] / n_j[i])
-            inf.append(sir_sim[1, i] / n_j[i])
-            r.append(sir_sim[2, i] / n_j[i])
-            d.append(0)
+        # 3.4 calculate new_infect
+        new_infect = np.round(beta_vec * sir_sim[:, 0] * inflow_infected
+                              / (n_j + od_flow.sum(axis=0) - od_flow.sum(axis=1)))
 
-        susceptible_pop_norm.append(sum(s) / len(s))
-        infected_pop_norm.append(sum(inf) / len(inf))
-        recovered_pop_norm.append(sum(r) / len(r))
-        dead_pop_norm.append(sum(d) / len(d))
+        # 3.5 set upper limit of the infected group (total susceptible)
+        new_infect = np.where(new_infect > sir_sim[:, 0], sir_sim[:, 0], new_infect)
 
+        # 3.6 calculate total number of people recovered
+        new_recovered = np.round(gamma_vec * sir_sim[:, 1])
+
+        # 3.7. calculate total death
+        dead = np.round(fatality_vec * sir_sim[:, 1])
+
+        # 3.8 remove new infections from susceptible group
+        sir_sim[:, 0] = sir_sim[:, 0] - new_infect
+
+        # 3.9 add new infections into infected group,
+        # also remove recovers from the infected group
+        # also remove the dead from the infected group
+        sir_sim[:, 1] = sir_sim[:, 1] + new_infect - new_recovered - dead
+
+        # add recovers to the recover group
+        sir_sim[:, 2] = sir_sim[:, 2] + new_recovered
+
+        # add dead to dead group
+        sir_sim[:, 3] = dead
+
+        # subtract dead from the population
+        n_j = n_j - dead
+
+        # set lower limits of the groups (0 people)
+        sir_sim = np.where(sir_sim < 0, 0, sir_sim)
+
+        # 3.10. compute the normalized SIR matrix
+        region_sum = sir_sim.sum(axis=0)
+        region_sum_normalized = region_sum / n_j.sum()
+        s = region_sum_normalized[0]
+        i = region_sum_normalized[1]
+        r = region_sum_normalized[2]
+        d = region_sum_normalized[3]
+        susceptible_pop_norm.append(s)
+        infected_pop_norm.append(i)
+        recovered_pop_norm.append(r)
+        dead_pop_norm.append(d)
 
     return [susceptible_pop_norm, infected_pop_norm, recovered_pop_norm,
             dead_pop_norm]
@@ -198,14 +225,38 @@ sir_simulation_plot(outcome, days)
 # Policy 3. reduce the o-d flow by 80%, all other arguments stay unchanged
 # Policy 4. reduce the o-d flow by 80%, reduce beta by 50%, all other the same
 
-outcome1 = epidemic_sim(n_j, sir, od_flow, alpha_vec, beta_vec, fatality_vec,
-                       gamma_vec, days=days)
-outcome2 = epidemic_sim(n_j, sir, 0.5, alpha_vec, beta_vec, fatality_vec,
-                       gamma_vec, days=days)
-outcome3 = epidemic_sim(n_j, sir, 0.8, alpha_vec, beta_vec, fatality_vec,
-                       gamma_vec, days=days)
-outcome4 = epidemic_sim(n_j, sir, 0.8, alpha_vec, np.full(len(n_j), 0.4), fatality_vec,
-                       gamma_vec, days=days)
+outcome1 = epidemic_sim(n_j.copy(),
+                        sir.copy(),
+                        od_flow,
+                        alpha_vec.copy(),
+                        beta_vec.copy(),
+                        fatality_vec.copy(),
+                        gamma_vec.copy(),
+                        days=days)
+outcome2 = epidemic_sim(n_j.copy(),
+                        sir.copy(),
+                        np.round(od_matrix)*0.8,
+                        alpha_vec.copy(),
+                        beta_vec.copy(),
+                        fatality_vec.copy(),
+                        gamma_vec.copy(),
+                        days=days)
+outcome3 = epidemic_sim(n_j.copy(),
+                        sir.copy(),
+                        np.round(od_matrix)*0.8,
+                        alpha_vec.copy(),
+                        beta_vec.copy(),
+                        fatality_vec.copy(),
+                        gamma_vec.copy(),
+                        days=days)
+outcome4 = epidemic_sim(n_j.copy(),
+                        sir.copy(),
+                        np.round(od_matrix)*0.5,
+                        alpha_vec.copy(),
+                        np.full(len(n_j), 0.4),
+                        fatality_vec.copy(),
+                        gamma_vec.copy(),
+                        days=days)
 
 fig, axs = plt.subplots(2, 2)
 
